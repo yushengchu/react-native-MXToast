55e78590f1a432dd883650f292d749a963fb76c4
jinweiyang <jinweiyang@51dojo.com>
2017-10-08 22:34:29 +0800

    修改rn端的方法调用方式，同时兼容android iOS两种原生调用

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-import {NativeModules} from 'react-native';
+import {NativeModules, Platform} from 'react-native';
 
-module.exports = NativeModules.MXToastBridge;
+let MXToastBridge = NativeModules.MXToastBridge;
+
+let MXToast = {
+    TOP: Platform.OS === 'android' ? MXToastBridge.TOP : 'TOP',
+    BOTTOM: Platform.OS === 'android' ? MXToastBridge.TOP : 'BOTTOM',
+    CENTER: Platform.OS === 'android' ? MXToastBridge.TOP : 'CENTER',
+    SHORT: Platform.OS === 'android' ? MXToastBridge.TOP : 'SHORT',
+    LONG: Platform.OS === 'android' ? MXToastBridge.TOP : 'LONG',
+    show: (msg, duration, gravity, margin) => {
+        MXToastBridge.show(msg, {duration, gravity, margin})
+    }
+};
+
+module.exports = MXToast;
